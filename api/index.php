aefc2c8539e85d00bbd56179ef4b347a7087ef63
karthikabaskar <karthika.baskar@fountaintechies.com>
2015-03-15 16:02:30 +0000

    Post bind param

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim();
 
 $app->get('/users', 'getUsers');
-$app->post('/user', 'insertUser');
+$app->post('/user', 'addUser');
 $app->run();
 
 //  https://task-manager-karthikabaskar-2.c9.io/api/users
@@ -26,17 +26,21 @@ function getUsers() {
 }
 
 // POST http://www.yourwebsite.com/api/updates
-function insertUser() {
-  $request = $app->request();
-  $user = json_decode($request->getBody());
-  $sql = "INSERT INTO users (`firstname`, `lastname`, `email`, `mobileno`) VALUES ( :fname,:lname, :mail, :mobileno);";
+function addUser() {
+  global $app;
+  $req = $app->request();//Getting parameter
+  $paramFName =  $req->params('fname');
+  $paramLName =  $req->params('lname');
+  $paramEmail =  $req->params('email');
+  $paramMobNo =  $req->params('mobno');
+  $sql = "INSERT INTO users (`firstname`, `lastname`, `email`, `mobileno`) VALUES ( :fname,:lname, :email, :mobno);";
   try {
     $db = getConnection();
     $stmt = $db->prepare($sql);
-    $stmt = bindParam("firstname", $user->fname);
-    $stmt = bindParam("lastname", $user->lname);
-    $stmt = bindParam("email", $user->mail);
-    $stmt = bindParam("mobileno", $user->mobileno);
+    $stmt = bindParam("firstname", $paramFName);
+    $stmt = bindParam("lastname", $paramLName);
+    $stmt = bindParam("email", $paramEmail);
+    $stmt = bindParam("mobileno", $paramMobNo);
     $stmt->execute();
     $db = null;
     echo json_encode($user);
